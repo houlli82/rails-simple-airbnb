6793a12aa2567482ab83481f8812a8d01a94c9f0
mark_houlli <markhoulli@protonmail.com>
2022-07-09 08:57:22 +0900

    need to add the add btn

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Destroying flats..."
+Flat.destroy_all
+puts "creating 10 flats..."
+10.times do
+  Flat.create(
+    name: Faker::Address.city,
+    address: Faker::Address.street_address,
+    description: Faker::Lorem.paragraph,
+    price_per_night: rand(100..999),
+    number_of_guests: rand(2..6)
+  )
+end
+puts "Seeding complete 🌱"
